3d47057b707ea072042a25845a10eb33ffea6e24
bjerrep <bjerre3@gmail.com>
2020-01-09 23:55:31 +0100

    client gpio mapping can use config file

--- src/client/gpio.py
+++ src/client/gpio.py
@@ -1,5 +1,6 @@
 from common.log import logger as log
 from enum import Enum
+import json
 
 
 try:
@@ -16,20 +17,57 @@ class LedColor(Enum):
     ORANGE = 3
 
 
-if GPIO:
-    GPIO.setmode(GPIO.BCM)
+GPIO_SPEAKER = 15
+GPIO_SPEAKER_ACTIVE_HIGH = True
+GPIO_PAPOWER = 14
+GPIO_PAPOWER_ACTIVE_HIGH = False
+GPIO_LED_A = 23
+GPIO_LED_A_ACTIVE_HIGH = True
+GPIO_LED_B = 22
+GPIO_LED_B_ACTIVE_HIGH = True
+GPIO_GPIO = 17
 
-    GPIO_SPEAKER = 15
-    GPIO_PAPOWER = 14
-    GPIO_LED_A = 23
-    GPIO_LED_B = 22
-    GPIO_GPIO = 17
 
-    GPIO.setup(GPIO_SPEAKER, GPIO.OUT)
-    GPIO.setup(GPIO_PAPOWER, GPIO.OUT)
-    GPIO.setup(GPIO_LED_A, GPIO.OUT)
-    GPIO.setup(GPIO_LED_B, GPIO.OUT)
-    GPIO.setup(GPIO_GPIO, GPIO.OUT)
+def init():
+    global GPIO_PAPOWER, GPIO_SPEAKER, GPIO_PAPOWER_ACTIVE_HIGH, GPIO_SPEAKER_ACTIVE_HIGH
+    global GPIO_LED_A, GPIO_LED_A_ACTIVE_HIGH, GPIO_LED_B, GPIO_LED_B_ACTIVE_HIGH
+    if GPIO:
+        try:
+            with open('client_hw_setup.cfg') as f:
+                config = json.loads(f.read())
+                try:
+                    GPIO_PAPOWER = config['papower']['pin']
+                    GPIO_PAPOWER_ACTIVE_HIGH = config['papower']['active']
+                except:
+                    pass
+                try:
+                    GPIO_SPEAKER = config['speaker']['pin']
+                    GPIO_SPEAKER_ACTIVE_HIGH = config['speaker']['active']
+                except:
+                    pass
+                try:
+                    GPIO_LED_A = config['ledA']['pin']
+                    GPIO_LED_A_ACTIVE_HIGH = config['ledA']['active']
+                except:
+                    pass
+                try:
+                    GPIO_LED_B = config['ledB']['pin']
+                    GPIO_LED_B_ACTIVE_HIGH = config['ledB']['active']
+                except:
+                    pass
+
+                log.debug(f'speaker on gpio={GPIO_SPEAKER} polarity={GPIO_SPEAKER_ACTIVE_HIGH}')
+                log.debug(f'papower on gpio={GPIO_PAPOWER} polarity={GPIO_PAPOWER_ACTIVE_HIGH}')
+        except:
+            log.info('using default i/o mappings')
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        GPIO.setup(GPIO_SPEAKER, GPIO.OUT)
+        GPIO.setup(GPIO_PAPOWER, GPIO.OUT)
+        GPIO.setup(GPIO_LED_A, GPIO.OUT)
+        GPIO.setup(GPIO_LED_B, GPIO.OUT)
+        GPIO.setup(GPIO_GPIO, GPIO.OUT)
 
 
 def close():
@@ -38,33 +76,39 @@ def close():
     GPIO.cleanup()
 
 
-def speaker_relay(on):
+def speaker(on):
+    # Off the shelf class-D amplifiers often have a 'enable' input which can bring
+    # the amplifier to sleep before cutting the power. Alternatively the speaker
+    # signal can be used to control standard relays. All this is done to avoid
+    # the risk of bangs and pops when power comes and goes.
     if not GPIO:
         return
-    GPIO.output(GPIO_SPEAKER, on)
+    GPIO.output(GPIO_SPEAKER, on if GPIO_SPEAKER_ACTIVE_HIGH else not on)
 
 
-def Power(on):
+def power(on):
+    # Power on/off the amplifier power supply, typically done with a solid state relay.
     if not GPIO:
         return
-    GPIO.output(GPIO_PAPOWER, not on)
+    log.debug(f'setting pa power {on}')
+    GPIO.output(GPIO_PAPOWER, on if GPIO_PAPOWER_ACTIVE_HIGH else not on)
 
 
 def LED(color):
     if not GPIO:
         return
     if color == LedColor.OFF:
-        GPIO.output(GPIO_LED_A, False)
-        GPIO.output(GPIO_LED_B, False)
+        GPIO.output(GPIO_LED_A, not GPIO_LED_A_ACTIVE_HIGH)
+        GPIO.output(GPIO_LED_B, not GPIO_LED_B_ACTIVE_HIGH)
     elif color == LedColor.RED:
-        GPIO.output(GPIO_LED_A, True)
-        GPIO.output(GPIO_LED_B, False)
+        GPIO.output(GPIO_LED_A, GPIO_LED_A_ACTIVE_HIGH)
+        GPIO.output(GPIO_LED_B, not GPIO_LED_B_ACTIVE_HIGH)
     elif color == LedColor.GREEN:
-        GPIO.output(GPIO_LED_A, False)
-        GPIO.output(GPIO_LED_B, True)
+        GPIO.output(GPIO_LED_A, not GPIO_LED_A_ACTIVE_HIGH)
+        GPIO.output(GPIO_LED_B, GPIO_LED_B_ACTIVE_HIGH)
     else:
-        GPIO.output(GPIO_LED_A, True)
-        GPIO.output(GPIO_LED_B, True)
+        GPIO.output(GPIO_LED_A, GPIO_LED_A_ACTIVE_HIGH)
+        GPIO.output(GPIO_LED_B, GPIO_LED_B_ACTIVE_HIGH)
 
 
 def GPIO_PIN(on):
--- src/client/hwctl.py
+++ src/client/hwctl.py
@@ -17,11 +17,15 @@ class HwCtl(util.Threadbase):
     def __init__(self):
         super(HwCtl, self).__init__(name='hwctl')
         self.target = HardwareState.BOOT
+        self.connected = False
         gpio.reset()
         self.start()
 
     def terminate(self):
         super().terminate()
+
+    def connected_to_server(self, connected):
+        self.connected = connected
 
     def play(self, on):
         if on:
@@ -37,33 +41,36 @@ class HwCtl(util.Threadbase):
             time.sleep(0.1)
 
             if self.target == HardwareState.PLAYING and state != HardwareState.PLAYING:
-                # immediately power on and in next pass energize the relays
+                # immediately power on and in next pass enable speakers
                 if poweroff_counter != 300:
                     poweroff_counter = 300
-                    gpio.Power(True)
-                    gpio.LED(gpio.LedColor.ORANGE)
+                    gpio.power(True)
                 elif poweroff_counter == 300:
-                    gpio.speaker_relay(True)
-                    gpio.LED(gpio.LedColor.GREEN)
+                    gpio.speaker(True)
                     state = HardwareState.PLAYING
 
             if self.target == HardwareState.POWEROFF and state != HardwareState.POWEROFF:
-                # a long count towards 1 and 0 for speaker relays off and then power off respectively
+                # a long count towards 1 and 0 for speakers off and then power off respectively
                 if poweroff_counter > 0:
                     poweroff_counter -= 1
                     if poweroff_counter == 1:
-                        gpio.speaker_relay(False)
-                        gpio.LED(gpio.LedColor.ORANGE)
+                        gpio.speaker(False)
                     elif poweroff_counter == 0:
-                        gpio.Power(False)
-                        gpio.LED(gpio.LedColor.RED)
+                        gpio.power(False)
                         state = HardwareState.POWEROFF
 
+            if self.connected:
+                if self.target == HardwareState.PLAYING:
+                    gpio.LED(gpio.LedColor.ORANGE)
+                else:
+                    gpio.LED(gpio.LedColor.GREEN)
+            else:
+                gpio.LED(gpio.LedColor.RED)
+
         if state != HardwareState.POWEROFF:
-            gpio.speaker_relay(False)
-            time.sleep(0.2)
-            gpio.Power(False)
-            gpio.LED(gpio.LedColor.RED)
+            gpio.speaker(False)
+            time.sleep(0.5)
+            gpio.power(False)
 
         gpio.close()
         log.debug('hwctrl exits')
